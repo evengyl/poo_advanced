b9562da94a2cd4b3e78b17bf4ac61729e54e45c3
evengyl <dark.evengyl@gmail.com>
2015-08-04 08:54:26 +0200

    petite modif

--- contents/listing_clients.php
+++ contents/listing_clients.php
@@ -14,7 +14,7 @@ $res_pdo = $pdo->query_pdo("SELECT * FROM marketing_matedex ORDER BY id", '\Even
                 <h2><?php echo html_entity_decode($articles->nom); ?></h2>
                 <p><?php echo $articles->adresse; ?></p>
                 <p><?php echo $articles->tel; ?></p>
-                <p><a href="<?php echo $articles->get_link_website(); ?>">Lien vers le site du client</a></p>
+                <p><a href="<?php echo $articles->link_website; ?>">Lien vers le site du client</a></p>
 
                 <p><a class="btn btn-default" href="#" role="button">Valider &raquo;</a>
                     <a class="btn btn-default" href="#" role="button">Laisser un commentaire &raquo;</a></p>
